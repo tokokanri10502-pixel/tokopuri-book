b1d8211eb542ecdf0edae43aeab12b057df829d8
tokokanri10502-pixel <tokokanri10502@gmail.com>
2026-03-30 09:18:44 +0900

    PWA本格化

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,16 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -6,4 +19,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
